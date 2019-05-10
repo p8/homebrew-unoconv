dd4a182414389a9d2861de86ae2a79c753885d59
Petrik <petrik@deheus.net>
2019-05-10 15:12:53 +0200

    Pin archive to commit sha

--- Formula/unoconv.rb
+++ Formula/unoconv.rb
@@ -1,7 +1,7 @@
 class Unoconv < Formula
   desc "Convert between any document format supported by OpenOffice"
   homepage "http://dag.wiee.rs/home-made/unoconv/"
-  url "https://github.com/unoconv/unoconv/archive/master.zip"
+  url "https://github.com/unoconv/unoconv/archive/63f6b13b6b027b9d552b445dacc8316d7a276043.zip"
   sha256 "2b5bf10177b1d14fe8521636e903dee6e6241c2d0b277ef205a771756e41bbb8"
   revision 2
   version "0.8.2"
